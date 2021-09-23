77f883de023145aef89edfd00190cf3432efee7d
xuzhongpeng <1452754335@qq.com>
2021-09-23 23:07:25 +0800

    fix example running bugs

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/admin/Desktop/Application/flutter"
+export "FLUTTER_ROOT=/Users/admin/fvm/versions/stable"
 export "FLUTTER_APPLICATION_PATH=/Users/admin/Desktop/company/new_flutter/qrcode/example"
-export "FLUTTER_TARGET=/Users/admin/Desktop/company/new_flutter/qrcode/example/lib/main.dart"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/admin/Desktop/Application/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=0.0.1"
-export "TRACK_WIDGET_CREATION=true"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
